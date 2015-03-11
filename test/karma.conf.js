73743e9406d84fbd807ebf503ee61e0626e8b4dc
Wesley Chin <wesley@databuild.co.za>
2015-03-11 08:58:33 +0200

    Added in extra stuff

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -19,6 +19,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       // bower:js
+      'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-cookies/angular-cookies.js',
@@ -28,6 +29,7 @@ module.exports = function(config) {
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-foundation/mm-foundation-tpls.js',
       'bower_components/highcharts-ng/dist/highcharts-ng.js',
+      'bower_components/lodash/lodash.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
