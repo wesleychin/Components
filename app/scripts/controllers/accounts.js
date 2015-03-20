619d14c508f7470561848f450fac3eaa1b88911f
Wesley Chin <wesley@databuild.co.za>
2015-03-20 15:04:22 +0200

    Added the Portfolio performance chart and did some tweaks to it

--- app/scripts/controllers/accounts.js
+++ app/scripts/controllers/accounts.js
@@ -8,61 +8,117 @@ angular.module('componentsApp')
 	$scope.idSelectedVote = 1;
 	$scope.displayDisclaimer = true;
 
-	$scope.sectorBreakdownChart = {
+	$scope.sectorBreakdownGrid = {
 		dataSource: {
-                type: "json",
-                transport: {
-                    read: "api/portfolio/sector-breakdown.json"
-                },
-                pageSize: 10
-            },
-            scrollable: false,
-		    resizable: true,
-		    reorderable: true,
-		    sortable: true,
-		    pageable: false,
-            columns: [{
-                field: "symbol",
-                title: " "
-            },{
-                field: "sectorName",
-                title: "Sector name"
-            },{
-                field: "cost",
-                title: "Cost"
+			type: "json",
+			transport: {
+				read: "api/portfolio/sector-breakdown.json"
+			},
+			pageSize: 10
+		},
+		scrollable: false,
+		resizable: true,
+		reorderable: true,
+		sortable: true,
+		pageable: false,
+		columns: [{
+			field: "symbol",
+			title: " "
+		},{
+			field: "sectorName",
+			title: "Sector name"
+		},{
+			field: "cost",
+			title: "Cost"
 
-            },{
-                field: "marketValue",
-                title: "Market value"
-            },{
-                field: "difference",
-                title: "% Difference (Difference)"
-            },{
-                field: "portfolio",
-                title: "% of portfolio"
-            }]
-	};
+		},{
+			field: "marketValue",
+			title: "Market value"
+		},{
+			field: "difference",
+			title: "% Difference (Difference)"
+		},{
+			field: "portfolio",
+			title: "% of portfolio"
+		}]
+	}
+
+	$scope.portfolioPerformanceChart = {
+		seriesDefaults: {
+			type: "column"
+		},
+		series: [{
+			color: function(data) {
+				if (data.value > 0) {
+					return "green";
+				} else {
+					return "red";
+				}
+			}, 
+			gap: 0,
+			spacing: 0,
+			data: [20,
+			-50,
+			-40,
+			-30,
+			40,
+			-80,
+			10,
+			-5,
+			60,
+			-60,
+			70,
+			-20]
+		}],
+		valueAxis: {
+			labels: {
+				format: "{0}%"
+			},
+			line: {
+				visible: false
+			},
+			axisCrossingValue: 0, 
+			majorGridLines: false
+		},
+		categoryAxis: {
+			categories: ["Jan", "Feb", "Mar", "Apr", "May", "June", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+			line: {
+				visible: false
+			},
+			labels: {
+				padding: {top: 200}
+			}, 
+			majorGridLines: false
+		},
+		tooltip: {
+			visible: true,
+			format: "{0}%",
+			template: "#= series.name #: #= value #"
+		},
+		chartArea: { margin: 0, padding: 0, height: (screen.height * 0.55), width: (screen.width * 0.57) },
+plotArea: { margin: 0, padding: 0, height: (screen.height * 0.55), width: (screen.width * 0.57) }
+	}
 
 	$scope.mainGridOptions = {
 		dataSource: {
-                type: "json",
-                transport: {
-                    read: "api/portfolio/getData.json"
-                },
-                pageSize: 10
-            },
-            sortable: true,
-            pageable: true,
-            columns: [{
-                field: "firstName",
-                title: "First Name"
-            },{
-                field: "lastName",
-                title: "Last Name"
+			type: "json",
+			transport: {
+				read: "api/portfolio/getData.json"
+			},
+			pageSize: 10
+		},
+		sortable: true,
+		pageable: true,
+		columns: [{
+			field: "firstName",
+			title: "First Name"
+		},{
+			field: "lastName",
+			title: "Last Name"
 
-            },{
-                field: "country"
-            }]
+		},{
+			field: "country"
+		}]
 	};
 
 	// $scope.detailGridOptions = function(dataItem) {
@@ -97,14 +153,6 @@ angular.module('componentsApp')
 
 	$http.get('/api/portfolio/portfolio-overview.json').success(function(response) {
 		$scope.JSONdata = response;
-	});
-
-	$http.get('/api/portfolio/market-news.json').success(function(response) {
-		$scope.marketNews = response;
-	});
-
-	$http.get('/api/portfolio/portfolio-dialog-buttons.json').success(function(response) {
-		$scope.dialogButtons = response;
 	});
 
 	$scope.goToView= function (sectionId) {
