8a0dcc86edb83f8704e1be1298cd242d757cb18e
Wesley Chin <wesley@databuild.co.za>
2015-03-25 09:56:25 +0200

    Added foundation and other buttons

--- app/scripts/controllers/accounts.js
+++ app/scripts/controllers/accounts.js
@@ -19,10 +19,7 @@ angular.module('componentsApp')
 	// 	$scope.portfolioPerformanceChart = response;
 	// });
 
-
 	$http.get('/api/portfolio/portfolio-performance.json').success(function (data, status) {
-		console.log(data.categories);
-		console.log(data.values);
 		$scope.chartConfig = {
 			chart: {
 				type: 'column'
@@ -37,94 +34,64 @@ angular.module('componentsApp')
 				enabled: false
 			}, 
 			plotOptions: {
-            column: {
-                pointPadding: 0.2,
-                borderWidth: 0
-            }
-        }
+				column: {
+					pointPadding: 0.2,
+					borderWidth: 0
+				}
+			}
 		};
 	});
 
-
-
-
-
-
-$scope.portfolioPerformanceChart = {
-	seriesDefaults: {
-		type: "column"
-	},
-	series: [{
-		color: function(data) {
-			if (data.value > 0) {
-				return "green";
-			} else {
-				return "red";
-			}
-		}, 
-		gap: 0,
-		spacing: 0,
-		data: [20,
-		-50,
-		-40,
-		-30,
-		40,
-		-80,
-		10,
-		-5,
-		60,
-		-60,
-		70,
-		-20]
-	}],
-	valueAxis: {
-		labels: {
-			format: "{0}%"
+	$scope.portfolioPerformanceChart = {
+		seriesDefaults: {
+			type: "column"
 		},
-		line: {
-			visible: false
+		series: [{
+			color: function(data) {
+				if (data.value > 0) {
+					return "green";
+				} else {
+					return "red";
+				}
+			}, 
+			gap: 0,
+			spacing: 0,
+			data: [20,
+			-50,
+			-40,
+			-30,
+			40,
+			-80,
+			10,
+			-5,
+			60,
+			-60,
+			70,
+			-20]
+		}],
+		valueAxis: {
+			labels: {
+				format: "{0}%"
+			},
+			line: {
+				visible: false
+			},
+			axisCrossingValue: 0, 
+			majorGridLines: false
 		},
-		axisCrossingValue: 0, 
-		majorGridLines: false
-	},
-	categoryAxis: {
-		categories: ["Jan", "Feb", "Mar", "Apr", "May", "June", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-		line: {
-			visible: false
+		categoryAxis: {
+			categories: ["Jan", "Feb", "Mar", "Apr", "May", "June", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+			line: {
+				visible: false
+			},
+			labels: {
+				padding: {top: 200}
+			}, 
+			majorGridLines: false
 		},
-		labels: {
-			padding: {top: 200}
-		}, 
-		majorGridLines: false
-	},
 		chartArea: { background: "#FFF", margin: 0, padding: 0, height: (screen.height * 0.48), width: (screen.width * 0.50) },
 		plotArea: { background: "#FFF", margin: 0, padding: 0, height: (screen.height * 0.48), width: (screen.width * 0.50) }
 	}
-
-	// $scope.detailGridOptions = function(dataItem) {
-	// 	return {
-	// 		dataSource: {
-	// 			type: "odata",
-	// 			transport: {
-	// 				read: "http://demos.telerik.com/kendo-ui/service/Northwind.svc/Orders"
-	// 			},
-	// 			serverPaging: true,
-	// 			serverSorting: true,
-	// 			serverFiltering: true,
-	// 			pageSize: 5,
-	// 			filter: { field: "EmployeeID", operator: "eq", value: dataItem.EmployeeID }
-	// 		},
-	// 		scrollable: false,
-	// 		sortable: true,
-	// 		pageable: true,
-	// 		columns: [
-	// 		{ field: "OrderID", title:"ID", width: "56px" },
-	// 		{ field: "ShipCountry", title:"Ship Country", width: "110px" },
-	// 		{ field: "ShipAddress", title:"Ship Address" },
-	// 		{ field: "ShipName", title: "Ship Name", width: "190px" }
-	// 		]
-	// 	};
-	// };
 
 	$http.get('/api/portfolio/market-news.json').success(function(response) {
 		$scope.marketNews = response;
