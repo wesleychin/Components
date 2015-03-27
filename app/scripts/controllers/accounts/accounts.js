c9368bcc80d61d3d6d70bc46167d4daaecdb136e
Wesley Chin <wesley@databuild.co.za>
2015-03-27 11:35:38 +0200

    Refactored

---
+++ app/scripts/controllers/accounts/accounts.js
@@ -0,0 +1,38 @@
+'use strict';
+
+angular.module('componentsApp')
+.controller('AccountsCtrl', function ($scope, $http, $timeout, $filter) {
+	$scope.view = 1;
+	$scope.time = moment().format('h:mm:ss');
+	$scope.date = moment().format('YYYY/MM/DD'); 
+	$scope.idSelectedVote = 1;
+
+	$http.get('/api/accounts/account-details.json').success(function(response) {
+		$scope.accounts = response;
+	});
+
+	$http.get('/api/common/download-options.json').success(function(response) {
+		$scope.downloadOptions = response;
+	});
+
+	$http.get('/api/common/search-options.json').success(function(response) {
+		$scope.searchOptions = response;
+	});
+
+	$http.get('/api/portfolio/portfolio-dialog-buttons.json').success(function(response) {
+		$scope.buttons = response;
+	});
+
+	$http.get('/api/portfolio/sections.json').success(function(response) {
+		$scope.sections = response;
+	});
+
+	$http.get('/api/portfolio/performers.json').success(function(response) {
+		$scope.performers = response;
+	});
+
+	$scope.goToView= function (sectionId) {
+		$scope.view = sectionId;
+		$scope.idSelectedVote = sectionId;
+	};
+});
---
+++ app/scripts/controllers/portfolio/sector-breakdown.js
@@ -0,0 +1,76 @@
+'use strict';
+
+angular.module('componentsApp')
+.controller('SectorBreakdownCtrl', function ($scope, $http, $timeout, $filter) {
+	$scope.sectorBreakdownGrid = {
+		dataSource:{  
+			type:"json",
+			transport:{  
+				read:{  
+					url:"api/portfolio/sector-breakdown.json"
+				}
+			},
+			serverPaging: true,
+			serverSorting: true
+		},
+		sortable: true,
+		pageable: false,
+		scrollable: false,
+		dataBound: function() {
+			this.expandRow(this.tbody.find("tr.k-master-row").first());
+		},
+		columns:[  
+		{  
+			field:"symbol",
+			title:" ",
+			template: '<span ng-class="{\'k-icon k-i-arrow-n\': dataItem.symbol == \'increase\', \'k-icon k-i-arrow-s\': dataItem.symbol == \'decrease\', \'k-icon k-i-arrow-e\': dataItem.symbol == \'neutral\'}"></span>'
+		},
+		{  
+			field:"sectorName",
+			title:"Sector name"
+		},
+		{  
+			field:"cost",
+			title:"Cost"
+		},
+		{  
+			field:"marketValue",
+			title:"Market value"
+		},
+		{  
+			field:"difference",
+			title:"% Difference (Difference)"
+		},
+		{  
+			field:"portfolio",
+			title:"% of portfolio"
+		}
+		]
+	}
+
+	$scope.detailGridOptions = function(dataItem) {
+		return {
+			dataSource: {
+				type:"json",
+				transport:{  
+					read:{  
+						url:"api/portfolio/sector-breakdown-additional-information.json"
+					}
+				},
+				serverPaging: true,
+				serverSorting: true,
+				serverFiltering: true,
+				filter: { field: "sectorId", operator: "eq", value: dataItem.sectorId }
+			},
+			scrollable: false,
+			sortable: true,
+			pageable: false,
+			columns: [
+			{ field: "OrderID", title:"ID", width: "56px" },
+			{ field: "ShipCountry", title:"Ship Country", width: "110px" },
+			{ field: "ShipAddress", title:"Ship Address" },
+			{ field: "ShipName", title: "Ship Name", width: "190px" }
+			]
+		};
+	};
+});		
