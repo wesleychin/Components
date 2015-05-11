b3758ec65cda4485be57a6de51f2efcf999c25a4
Wesley Chin <wesley@databuild.co.za>
2015-05-11 10:13:27 +0200

    Removed a lot of unneccessary styles from the main Nigeria css

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,27 +8,27 @@
  *
  * Main module of the application.
  */
-angular
-  .module('componentsApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-	'ui.select',
-    'ngSanitize',
-    'ngTouch',
-    'mm.foundation', 
-    'kendo.directives', 
-    'highcharts-ng', 
-    'angularSpinner',
-    'ui.multiselect'
+ angular
+ .module('componentsApp', [
+  'ngAnimate',
+  'ngCookies',
+  'ngResource',
+  'ngRoute',
+  'ui.select',
+  'ngSanitize',
+  'ngTouch',
+  'mm.foundation', 
+  'kendo.directives', 
+  'highcharts-ng', 
+  'angularSpinner',
+  'ui.multiselect'
   ])
-  .config(function ($routeProvider, uiSelectConfig) {
-    uiSelectConfig.theme='select2';
-	uiSelectConfig.resetSearchInput = true;
-	$routeProvider
-      .when('/', {
-        templateUrl: 'views/accounts/accounts-overview.html',
-        controller: 'AccountsCtrl'
-      });
+ .config(function ($routeProvider, uiSelectConfig) {
+  uiSelectConfig.theme='select2';
+  uiSelectConfig.resetSearchInput = true;
+  $routeProvider
+  .when('/', {
+    templateUrl: 'views/accounts/accounts-overview.html',
+    controller: 'AccountsCtrl'
   });
+});
